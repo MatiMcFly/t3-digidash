1b4af22cbf1a1c15d534d8570f33d8f8935d8ad7
LENSLIM5RINALDO\rinal <rinaldoleone@hotmail.ch>
2026-05-30 09:04:44 +0200

    [fw/esp-remotexy] modified UI widgets

--- fw/esp/main/remotexy_protocol.h
+++ fw/esp/main/remotexy_protocol.h
@@ -7,8 +7,7 @@
 void remotexy_init(void);
 void remotexy_set_connected(bool connected);
 void remotexy_set_notify_enabled(bool enabled);
-void remotexy_set_instrument(float value);
-void remotexy_set_outputs(uint8_t led_01, uint8_t led_02, float instrument_01,
-						  float onlineGraph_01_var1, uint8_t led_03, uint8_t led_04,
-						  float linearbar_01, int16_t linearbar_02);
+void remotexy_set_outputs(uint8_t turn_signal_w, uint8_t high_beam_w, int16_t rpm_w,
+                          float batt_volt_w, uint8_t oil_03_w, uint8_t oil_18_w,
+                          int8_t water_temp_w, int8_t tank_level_w);
 void remotexy_handle_rx(const uint8_t *data, size_t len);
