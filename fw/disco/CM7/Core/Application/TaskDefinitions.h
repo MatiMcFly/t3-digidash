e875eebdb967206c31ff53a31719761d37da66b4
Matthias Schär <mati.schaer95@gmail.com>
2026-06-06 10:37:38 +0200

    [fw/disco/rtos] moved task definitionsinto separate file

---
+++ fw/disco/CM7/Core/Application/TaskDefinitions.h
@@ -0,0 +1,20 @@
+//     * **************************************************************** *
+//   *                                                                      *
+// *          (C) Copyright Matthias Schär, all rights reserved               *
+//   *                                                                      *
+//     * **************************************************************** *
+/// @file TaskDefinitions.h
+
+/// @brief Defines task stack sizes and priorities for the application tasks.
+#ifndef __TASK_DEFINITIONS_H__
+#define __TASK_DEFINITIONS_H__
+
+/** *** TASK STACK SIZES *** */
+#define TOUCHGFX_TASK_STACK_SIZE (4096U  / sizeof(StackType_t))
+#define APP_TASK_STACK_SIZE       (512U  / sizeof(StackType_t))
+
+/** *** TASK PRIORITIES *** */
+#define TOUCHGFX_TASK_PRIORITY   (tskIDLE_PRIORITY + 2)
+#define APP_TASK_PRIORITY        (tskIDLE_PRIORITY + 1)
+
+#endif /* __TASK_DEFINITIONS_H__ */
